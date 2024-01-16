7bf80b442312a56dfa55267499acca80e4cba0ab
Muthu Kumar, Rathina Priya <RathinaPriya_MuthuKumar@comcast.com>
2024-01-16 18:55:13 +0530

    Skipping Commit [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 //Comment
-//Another comment
+//Another Comment
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
